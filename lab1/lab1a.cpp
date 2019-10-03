9ea89041b100112d025f693dd11dfb0e4c4b6248
006185656 <006185656@csusb.edu@jb358-9.cse.csusb.edu>
2019-10-02 18:58:27 -0700

    arranged files and folders

---
+++ lab1/lab1a.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std; 
+
+int main(){
+	cout << "hello, San Bernardino!" << endl; 
+
+	
+
+	return 0; 
+}
---
+++ lab2/lab1b_ex3.cpp
@@ -0,0 +1,16 @@
+// <<006185656>> LAB 1B - Exercise 3
+#include <iostream>
+
+using namespace std; 
+
+int main(){
+	int n = 0; 
+	for(int i=1; i <=10; i++){
+		n+=i; 
+		//cout << n << endl;
+	}
+	
+	cout << n << endl; 
+
+	return 0; 
+}
