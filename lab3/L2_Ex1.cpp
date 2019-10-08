334b800458bb53c68b26de0ddb9b85e3c3bbd0e0
Akinyemi <junior2inf@yahoo.com>
2019-10-08 16:40:58 -0700

    started lab 3

---
+++ lab3/L2_Ex1.cpp
@@ -0,0 +1,31 @@
+/* 
+
+CSE201-F19 Tuesday or Thursday Lab (depending on your lab day) 
+
+@file L2_Ex1.cpp 
+@author Akinyemi Adetunji 
+@date October 8, 2019 
+DESCRIPTION: This program extracts characters from the
+input string = "Four score and seven years ago our fathers" 
+to make and print the output string = "carefree and no tears" 
+ALGORITHM:
+Get the input string 
+Form the output string 
+Extract and concatenate the appropriate characters 
+Print the output string
+
+ */ 
+
+
+#include <iostream> 
+#include <string>
+
+using namespace std; 
+
+int main(){
+	input_string = "Four score and seven years ago our fathers"
+	//output_string = "carefree and no tears"
+	
+
+	return 0; 
+}
---
+++ lab3/L2_Ex2.cpp
@@ -0,0 +1,33 @@
+/* 
+
+CSE201-F19 Tuesday or Thursday Lab (depending on your lab day) 
+@file L2_Ex2.cpp 
+@author Akinyemi Adetunji 
+@date October 8, 2019 
+DESCRIPTION: This program prompts the user for 3 double precision floating point values num1, num2, num3 and computes and displays the following values. 
+the sum of num1, num2, num3 
+the average of num1, num2 and num3 
+the log2 of num1 
+ALGORITHM: 
+Get three double precision floating point numbers 
+Prompt 
+Get the three numbers 
+Compute and print 
+Sum of the three numbers 
+Average of the three numbers 
+log2 of the first number 
+
+*/ 
+
+#include <iostream>
+#include <cmath>
+
+using namespace std; 
+
+int main()
+{
+	double num1, num2, num3; 	
+
+
+	return 0;
+}
