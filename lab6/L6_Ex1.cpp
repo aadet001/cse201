2327bc6c940d2dda61151ae32c8d6b5b078acd88
Akinyemi <junior2inf@yahoo.com>
2019-11-06 12:57:20 -0800

    started lab 6

---
+++ lab6/L6_Ex1.cpp
@@ -0,0 +1,33 @@
+/*
+
+CSE201-F19 Tuesday Lab (depending on your lab day)
+
+@file L6_Ex1.cpp
+
+@author Akinyemi Adetunji 
+@date November day, 2019
+
+DESCRIPTION: <provide here a brief and concise description of what this program does>
+
+ALGORITHM:
+
+<provide an algorithm that you use to solve this problem>
+
+ 
+
+*/
+#include <iostream> 
+
+using namespace std; 
+
+int main(){
+
+
+
+
+
+
+
+
+	return 0; 
+}
