e9d745aca7806d29b58a841534b86d24232e1736
adamant <junior2inf@yahoo.com>
2019-11-13 08:08:35 -0800

    started lab 7

---
+++ lab7/L7_Ex1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std; 
+
+int main(){
+	string month[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+	int vol[] = {4,6,2,8,10,2,2,10,12,16,8,4};
+
+	
+	return 0; 
+}
