1834fb0fb8f674de6ce022d39865eabed7e5ba31
Akinyemi <junior2inf@yahoo.com>
2019-11-15 00:39:17 -0800

    updates lab 7

--- lab7/L7_Ex1.cpp
+++ lab7/L7_Ex1.cpp
@@ -7,6 +7,16 @@ int main(){
 	string month[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
 	int vol[] = {4,6,2,8,10,2,2,10,12,16,8,4};
 
+	cout << "  ** Sales Statistics **" << endl; 
+	cout << "Month  Volume" << endl; 
+
+	for(int i =0; i < 12; i++){
+		cout << " " <<  month[i] <<  "   ";
+		for(int j =0; j < (vol[i]/2); j++){
+			cout << "*";
+		} 
+		cout << endl; 
+	}
 	
 	return 0; 
 }
